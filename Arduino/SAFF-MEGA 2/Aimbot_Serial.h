a3002675037c0fa53ebc17deb2f2699324f1baa5
Pravuz <situz.sj@gmail.com>
2015-03-24 13:36:45 +0100

    Aimbot_Serial
    Redesigned, buffer is now dynamic

--- Arduino/SAFF-MEGA 2/Aimbot_Serial.h
+++ Arduino/SAFF-MEGA 2/Aimbot_Serial.h
@@ -1,7 +1,6 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-#define BUF_SIZE 6 //todo: remove this stupid shit
 #define BAUDRATE 115200
 
 enum CMD_ID{
@@ -27,135 +26,117 @@ enum CMD_ID{
 #include <Arduino.h>
 
 bool debug = DEBUG;
+
 struct AimBot_Serial
 {
-
 	AimBot_Serial(HardwareSerial *serial){
 		m_serial = serial;
 		m_serial->begin(BAUDRATE);
+		m_rx = new uint8_t[];
+		m_tx = new uint8_t[];
 	}
 
 	void serialUpdate(){
+		len = 0;
 		if (m_serial->available()){
 			sync();
-			for (int i = 0; i < BUF_SIZE; i++){
-				m_buf[i] = m_serial->read();
+			while (true){
+				m_rx[len] = m_serial->read();
+				if (m_serial->peek() != AIM_SYNC) len++;
+				else break;
 			}
 		}
 		if (debug){
-			Serial.println("Serial synchronization complete");
+			Serial.println("Serial update complete");
 		}
-#if 0
-		//RECIEVE COMMANDS
-		switch (m_buf[1])
-		{
-		case POS_REACHED:
-			break;
-		default:
-			break;
-		}
-#endif
 	}
 
 	char getX(){
 		if (debug){
 			Serial.print("Recieved X-Vector: ");
-			Serial.println((char)m_buf[2]);
+			Serial.println((char)m_rx[2]);
 		}
-		if (m_buf[1] == VECTOR)	return (char)m_buf[2];
+		if (m_rx[1] == VECTOR)	return (char)m_rx[2];
 		else return 0;
 	}
 
 	char getY(){
 		if (debug){
 			Serial.print("Recieved Y-Vector: ");
-			Serial.println((char)m_buf[3]);
+			Serial.println((char)m_rx[3]);
 		}
-		if (m_buf[1] == VECTOR)	return (char)m_buf[3];
+		if (m_rx[1] == VECTOR)	return (char)m_rx[3];
 		else return 0;
 	}
 
 	bool isRCmode(){	// check if last recieved coords are RC or pixy-vector
 		if (debug){
 			Serial.print("Mode RC? : ");
-			Serial.println((char)m_buf[1]);
+			Serial.println((char)m_rx[1]);
 		}
-		if (m_buf[1] == VECTOR)	return false;
+		if (m_rx[1] == VECTOR)	return false;
 		else return true;
 	}
 
 	void sendVect(char x, char y){
-		m_rxbuf[0] = AIM_SYNC;
-		m_rxbuf[1] = VECTOR;
-		m_rxbuf[2] = x;
-		m_rxbuf[3] = y;
-		for (int i = 4; i < BUF_SIZE; i++){
-			m_rxbuf[i] = 0;
-		}
+		m_tx[0] = AIM_SYNC;
+		m_tx[1] = VECTOR;
+		m_tx[2] = x;
+		m_tx[3] = y;
 		if (debug){
 			Serial.println("Sending Vector");
-			for (int i = 0; i < BUF_SIZE; i++) Serial.print(m_rxbuf[i]);
-			Serial.println();
+			m_serial->write(m_tx, sizeof(uint8_t) * 4);
 		}
-		m_serial->write(m_rxbuf, BUF_SIZE);
+		m_serial->write(m_tx, sizeof(uint8_t) * 4);
 	}
 	void sendRCxy(char x, char y){
-		m_rxbuf[0] = AIM_SYNC;  // Sync
-		m_rxbuf[1] = MOV_XY;  // Signal the brugi it's in RC mode 
-		m_rxbuf[2] = x;
-		m_rxbuf[3] = y;
-		for (int i = 4; i < BUF_SIZE; i++){  // Fill the rest of the buffer with 0
-			m_rxbuf[i] = 0;	
-		}
+		m_tx[0] = AIM_SYNC;  // Sync
+		m_tx[1] = MOV_XY;  // Signal the brugi it's in RC mode 
+		m_tx[2] = x;
+		m_tx[3] = y;
 		if (debug){
 			Serial.println("Sending RC xy");  // For debug
-			for (int i = 0; i < BUF_SIZE; i++) Serial.print(m_rxbuf[i]);
+			m_serial->write(m_tx, sizeof(uint8_t)* 4);
 			Serial.println();
 		}
-		m_serial->write(m_rxbuf, BUF_SIZE);  // Write to brugi
+		m_serial->write(m_tx, sizeof(uint8_t)* 4);  // Write to brugi
 	}
 
 	void startPixy(){
-		m_rxbuf[0] = AIM_SYNC;
-		m_rxbuf[1] = PIXY_START;
-		for (int i = 2; i < BUF_SIZE; i++){
-			m_rxbuf[i] = 0;
-		}
+		m_tx[0] = AIM_SYNC;
+		m_tx[1] = PIXY_START;
 		if (debug){
 			Serial.print("Pixy command START: ");
-			Serial.println(m_rxbuf[1]);
+			Serial.println(m_tx[1]);
 		}
-		m_serial->write(m_rxbuf, BUF_SIZE);
+		m_serial->write(m_rx, sizeof(uint8_t)* 2);
 	}
-	
+
 	void stopPixy(){
-		m_rxbuf[0] = AIM_SYNC;
-		m_rxbuf[1] = PIXY_STOP;
-		for (int i = 2; i < BUF_SIZE; i++){
-			m_rxbuf[i] = 0;
-		}
+		m_tx[0] = AIM_SYNC;
+		m_tx[1] = PIXY_STOP;
 		if (debug){
 			Serial.print("Pixy command STOP: ");
-			Serial.println(m_rxbuf[1]);
+			Serial.println(m_tx[1]);
 		}
-		m_serial->write(m_rxbuf, BUF_SIZE);
+		m_serial->write(m_tx, sizeof(uint8_t)* 2);
 	}
 
 	void sendPosReached(){
-		m_rxbuf[0] = AIM_SYNC;
-		m_rxbuf[1] = POS_REACHED;
-		for (int i = 2; i < BUF_SIZE; i++){
-			m_rxbuf[i] = 0;
-		}if (debug){
+		m_tx[0] = AIM_SYNC;
+		m_tx[1] = POS_REACHED;
+		if (debug){
 			Serial.print("ESC Reached position ");
-			Serial.println(m_rxbuf[1]);
+			Serial.println(m_tx[1]);
 		}
-		m_serial->write(m_rxbuf, BUF_SIZE);
+		m_serial->write(m_tx, sizeof(uint8_t)* 2);
 	}
-	
+
 	void flushBuf(){
-		delete[] m_rxbuf, m_buf;
+		delete[] m_tx, m_rx;
 		m_serial->flush();
+		m_rx = new uint8_t[];
+		m_tx = new uint8_t[];
 	}
 
 private:
@@ -172,10 +153,9 @@ private:
 	}
 
 	HardwareSerial *m_serial;
-	uint8_t *m_buf;
-	uint8_t *m_rxbuf;
+	uint8_t *m_tx, *m_rx;
+	unsigned int len;
 };
-
 #endif
 #ifdef __PIXY
 //todo: move code from greyshades to here
