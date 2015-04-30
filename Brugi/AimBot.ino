0d8e2e78b0718e3791d6b004cdd4780af1dfb50b
Pravuz <situz.sj@gmail.com>
2015-04-30 11:05:41 +0200

    Variable speed depending on progress in movement

--- Brugi/AimBot.ino
+++ Brugi/AimBot.ino
@@ -62,13 +62,17 @@ void loop()
 	}
 	//when in RC-mode, we want to continually move regardless of the success of serial update. 
 	if (rc_mode) moveWithSpeed();
+	else{
+		MoveMotorPosSpeed(motorNumberYaw, y_Motor, 100);
+		MoveMotorPosSpeed(motorNumberPitch, z_Motor, 100);
+	}
 }
 
 void moveToPos(){
 
 	//converting angle to motor steps.
-	z_Pos_Steps = z_Pos != 0 ? z_Pos / bldc2 : z_Pos_Steps = 0; //todo: OPTIMALISER.
-	y_Pos_Steps = y_Pos != 0 ? y_Pos / bldc1 : y_Pos_Steps = 0;
+	z_Pos_Steps = z_Pos / bldc2;
+	y_Pos_Steps = y_Pos / bldc1;
 
 	int16_t z_pos_origin = z_Pos_Steps;
 	int16_t y_pos_origin = y_Pos_Steps;
@@ -116,6 +120,7 @@ void moveToPos(){
 			MoveMotorPosSpeed(motorNumberYaw, y_Motor, MOTORPOWER);
 		}
 	}
+	delay(100); //wait for rig to settle a little
 	megaSerial.sendPosReached();
 }
 
@@ -123,7 +128,6 @@ void moveWithSpeed(){
 
 	if ((z_Pos >= -1 && z_Pos <= 1)) z_Pos = 0;
 	if ((y_Pos >= -1 && y_Pos <= 1)) y_Pos = 0;
-	//if (!(z_Pos | y_Pos)) return; // z and y is 0, nothing to do
 
 	if (motor_0_update){
 		motor_0_update = false;
@@ -150,10 +154,12 @@ void setMotorFreq(int16_t &origin, int16_t &progress, uint8_t motorNumber){
 	switch (motorNumber)
 	{
 	case motorNumberPitch:
-		motor_0_freq = origin > 0 ? map(progress, 0, origin, MOTOR_0_MIN_FREQ, MOTOR_0_MAX_FREQ) : map(progress, origin, 0, MOTOR_0_MIN_FREQ, MOTOR_0_MAX_FREQ);
+		if ((origin/2)<progress) motor_0_freq = origin > 0 ? map(progress, 0, origin, MOTOR_0_MAX_FREQ, MOTOR_0_MIN_FREQ) : map(progress, origin, 0, MOTOR_0_MAX_FREQ, MOTOR_0_MIN_FREQ);
+		else motor_0_freq = origin > 0 ? map(progress, 0, origin, MOTOR_0_MIN_FREQ, MOTOR_0_MAX_FREQ) : map(progress, origin, 0, MOTOR_0_MIN_FREQ, MOTOR_0_MAX_FREQ);
 		break;
 	case motorNumberYaw:
-		motor_1_freq = origin > 0 ? map(progress, 0, origin, MOTOR_1_MIN_FREQ, MOTOR_1_MAX_FREQ) : map(progress, origin, 0, MOTOR_1_MIN_FREQ, MOTOR_1_MAX_FREQ);
+		if ((origin/2)<progress) motor_1_freq = origin > 0 ? map(progress, 0, origin, MOTOR_1_MAX_FREQ, MOTOR_1_MIN_FREQ) : map(progress, origin, 0, MOTOR_1_MAX_FREQ, MOTOR_1_MIN_FREQ);
+		else motor_1_freq = origin > 0 ? map(progress, 0, origin, MOTOR_1_MIN_FREQ, MOTOR_1_MAX_FREQ) : map(progress, origin, 0, MOTOR_1_MIN_FREQ, MOTOR_1_MAX_FREQ);
 		break;
 	default:
 		break;
